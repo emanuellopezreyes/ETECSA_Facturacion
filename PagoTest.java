0a0b59e7f488d8ab2e1f237e1ecc752822b9feaf
carlos10alonso <carlosmanuelalonsoperez@gmail.com>
2025-06-20 20:32:48 -0400

    Add files via upload

---
+++ PagoTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PagoTest {
+    @Test
+    void testPagoExitoso() {
+        ETECSAFacturacionService servicio = new ETECSAFacturacionService();
+        Factura factura = new Factura("F001", "C001", 100.0, new Date());
+        servicio.registrarFactura(factura);
+        
+        assertTrue(servicio.realizarPago("F001"));
+        assertTrue(factura.isPagada());
+    }
+
+    @Test
+    void testPagoFacturaInexistente() {
+        ETECSAFacturacionService servicio = new ETECSAFacturacionService();
+        assertFalse(servicio.realizarPago("F999"));
+    }
+}
